dbbc07cd084e0ce13c92357589b42dc5b051772e
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-03-28 19:10:59 -0300

    Criação da página Home #20

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 import styles from './Footer.module.css'
 
@@ -11,6 +11,9 @@ function Footer() {
                 </li>
                 <li>
                     <FaInstagram />
+                </li>
+                <li>
+                    <FaTwitter />
                 </li>
                 <li>
                     <FaLinkedin />
