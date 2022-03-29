b68103ead007403cd85a449549cfb832360761ba
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-03-28 22:32:19 -0300

    Componentização de formulário - #22

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,14 @@
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
+
 function NewProject() {
     return (
-        <h1>Novo Projeto</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>Formulário</p>
+            <ProjectForm btnText="Criar Projeto" />
+        </div>
     )
 }
 
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,17 @@
+import styles from "./ProjectForm.module.css"
+import Input from "../form/Input"
+import Select from "../form/Select"
+import SubmitButton from "../form/SubmitButton"
+
+function ProjectForm({btnText}) {
+    return (
+        <form className={styles.form}>
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total" />
+            <Select name="category_id" text="Selecione a categoria" />
+            <SubmitButton text={btnText} />
+        </form>
+    )
+}
+
+export default ProjectForm
