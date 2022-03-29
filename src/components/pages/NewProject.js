9a654efa4794c85af74379447a3c1c9f010e22d8
João Luiz Ramos <joaoluizramos96@gmail.com>
2022-03-28 23:04:34 -0300

    Removendo <p>

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -6,7 +6,6 @@ function NewProject() {
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>Formulário</p>
             <ProjectForm btnText="Criar Projeto" />
         </div>
     )
